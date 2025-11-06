fb14a0cb11bd0673e5764fbf9d191bca06146ab5
arthur <kokilanoisette@gmail.com>
2025-11-06 13:37:36 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Laboratoire prêt !");
+
+const nom = "Arthur";
+let age = 16;
+
+console.log(age);
+
+
+const motDePasseAttendu = "bisous1234";
+let motDePasseUtilisateur = "171109";
+
+if (motDePasseAttendu = bisous1234 ) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (caractéres < 8)  {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}git add . 
